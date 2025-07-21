4342b57d8bb35de6a1dc9950a4e7c6d55af92c04
yashika09 <yashikachooni30@gmail.com>
2025-07-21 15:42:29 +0530

    Updated Author schema

--- backend/model/Author.js
+++ backend/model/Author.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const authorSchema = new mongoose.Schema({
-  id: {type : String , requires:true , unique:true},
+  authorId: {type : String , requires:true , unique:true},
   name: { type: String, required: true, unique: true }
 });
 
