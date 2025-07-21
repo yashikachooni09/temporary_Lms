4cd9c0ab88b12bf079f73140ddf69e1b92c83016
Akshay0922 <akshayrealvlogs@gmail.com>
2025-07-21 17:55:49 +0530

    some changes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,8 @@
 const express=require("express")
 const router=express.Router()
+
 // const verifyToken=require("../middleware/authMiddleware")
+
 const controller=require("../controller/adminController")
 router.post("/signup",controller.signup)
 router.post("/login",controller.login)
