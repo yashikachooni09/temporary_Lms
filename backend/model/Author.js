fce1f71323446f136f86680f4657a03c82fd6425
yashika09 <yashikachooni30@gmail.com>
2025-07-21 23:12:17 +0530

    some changes

--- backend/model/Author.js
+++ backend/model/Author.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const authorSchema = new mongoose.Schema({
   authorId: {type : String , requires:true , unique:true},
-  name: { type: String, required: true, unique: true }
+  name: { type: String, required: true}
 });
 
 module.exports = mongoose.model("Author", authorSchema);
