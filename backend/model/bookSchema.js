b1eda69bd31d4c016733bec2b4395f8e7c7be0db
Mandeep kaur <1402mandeepkaur@gmail.com>
2025-07-21 15:19:54 +0530

    bookschema

---
+++ backend/model/bookSchema.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  bookId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  title: {
+    type: String,
+    required: true
+  },
+
+  authors: [
+    {
+      type: String, 
+      ref: "Author",
+      required: true
+    }
+  ],
+
+  edition: String,
+  volume: String,
+  publisher: String,
+  publishedYear: String, 
+  pages: Number,   
+
+  entryDate: {
+    type: String,
+    required: true
+  },
+
+  billNumber: String,
+  billDate: String, 
+  vendorName: String,
+  costOnBill: Number, 
+
+  department: String, 
+  copyCount: {
+    type: Number,
+    default: 0
+  },
+
+  image: String 
+});
+
+module.exports = mongoose.model("Book", bookSchema);
