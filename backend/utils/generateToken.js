d898592c5dbd66ed66becfcb9f4edd3ecd2ab720
Mandeep kaur <1402mandeepkaur@gmail.com>
2025-07-22 23:25:06 +0530

    token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,5 +1,5 @@
 const jwt=require("jsonwebtoken")
 const generateToken=(user)=>{
-    return jwt.sign({id:user._id,role:user.role},process.env.JWT_SECRET,{expiresIn:"1h"})
+    return jwt.sign({id:user._id,role:user.role,name:user.userName},process.env.JWT_SECRET,{expiresIn:"1h"})
 }
 module.exports=generateToken
