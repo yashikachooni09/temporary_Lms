a60cf7764e96fd606280289b4fcc2a34b30233bf
yashika09 <yashikachooni30@gmail.com>
2025-07-23 00:21:32 +0530

    change

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,4 +2,4 @@ const jwt=require("jsonwebtoken")
 const generateToken=(user)=>{
     return jwt.sign({id:user._id,role:user.role,name:user.userName},process.env.JWT_SECRET,{expiresIn:"1h"})
 }
-module.exports=generateToken
+module.exports=generateToken;
