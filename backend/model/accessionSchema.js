15d6fcd046093589712f2a9d8cd3bde1abcc6b62
yashika09 <yashikachooni30@gmail.com>
2025-07-24 17:43:35 +0530

    updating acession schema

--- backend/model/accessionSchema.js
+++ backend/model/accessionSchema.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 
 const accessionSchema = new mongoose.Schema({
-  department: { type: String, required: true, unique: true },  
-  startRange: { type: Number, required: true },                
-  current: { type: Number, required: true }                  
+  department: {
+    type: String,
+    required: true,
+    unique: true, // One entry per department
+  },
+  startRange: {
+    type: Number,
+    required: true,
+  },
+  endRange: {
+    type: Number,
+    required: true,
+  },
+  current: {
+    type: Number,
+    required: true,
+  }
 });
 
 module.exports = mongoose.model("AccessionNumber", accessionSchema);
