9e12da80ea4d246bb256d570b9e2be7ac8ec2d12
yashika09 <yashikachooni30@gmail.com>
2025-07-28 15:23:01 +0530

    dashborar

---
+++ backend/controller/DashboardController.js
@@ -0,0 +1,17 @@
+const Book = require("../model/bookSchema");
+const issueBook = require("../model/issueSchema");
+
+exports.totalStats = async(req,res)=>
+{
+     try {
+    const totalBooks = await Book.countDocuments();
+    const issuedBooks = await issueBook.countDocuments({ status: 'issued' });
+    const returnedBooks = await issueBook.countDocuments({ status: 'returned' });
+    const overdueBooks = await issueBook.countDocuments({ status: 'overdue' });
+
+    res.json({ totalBooks, issuedBooks, returnedBooks, overdueBooks });
+  } catch (error) {
+    console.error("Error in totalStats:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
